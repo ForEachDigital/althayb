2a6fd80345284b3da541e5e2a45f9d7882202feb
kareemAboelatta <kareemaboelatta@gmail.com>
2025-06-20 17:13:26 +0300

    working on project details.

--- app/[locale]/projects/Meta.tsx
+++ app/[locale]/projects/Meta.tsx
@@ -1,21 +1,19 @@
-
-
-import { MapPin, CalendarClock } from "lucide-react";
-import { format } from "date-fns";
+import {MapPin, CalendarClock} from "lucide-react";
+import {format} from "date-fns";
 
 interface Props {
     date: string;
     location: string;
 }
 
-export default function ProjectMeta({ date, location }: Props) {
+export default function ProjectMeta({date, location}: Props) {
     return (
-        <div className="flex flex-wrap items-center gap-6 text-sm text-gray-500">
-      <span className="flex items-center gap-1">
-        <MapPin className="h-4 w-4 text-accent" /> {location}
+        <div className="flex flex-wrap items-center gap-6 py-5 text-sm text-gray-500">
+      <span className="flex text-lg font-montserrat items-center gap-1">
+        <MapPin className="h-4 w-4 text-accent"/> {location}
       </span>
-            <span className="flex items-center gap-1">
-        <CalendarClock className="h-4 w-4 text-accent" />{" "}
+            <span className="flex  text-lg font-montserrat items-center gap-1">
+        <CalendarClock className="h-4 w-4  text-accent"/>{" "}
                 {format(new Date(date), "yyyy-MM-dd")}
       </span>
         </div>
